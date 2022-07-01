5dc8c165c1d2e01087ed2260194c8fb8ed02528a
akshatsolulab <akshat@solulab.com>
2022-07-01 19:12:27 +0530

    change

--- test/stakingTest.js
+++ test/stakingTest.js
@@ -5,7 +5,7 @@ const { ethers, upgrades } = require('hardhat');
 describe("Staking Contract", async () => {
 
     let Contract;
-    let contract;
+    let stak;
     let perkTokenContract;
     let myTokenContract;
     const chainlinkAggregatorAddress = "0x2bA49Aaa16E6afD2a993473cfB70Fa8559B523cF";
@@ -46,22 +46,22 @@ describe("Staking Contract", async () => {
 
         before("Deposit tokens to staking contract", async function() {
             const [owner] = await ethers.getSigners();
-            await myTokenContract.approve(contract.address, ethers.utils.parseEther('100000000000000000000000'));
+            await myTokenContract.approve(stak.address, ethers.utils.parseEther('100000000000000000000000'));
 
             amountToDeposit = ethers.utils.parseEther('90');
-            contractBalanceBeforeDeposit = await contract.totalStakedInPool();
-            await contract.depositInPool(amountToDeposit);
+            contractBalanceBeforeDeposit = await stak.totalStakedInPool();
+            await stak.depositInPool(amountToDeposit);
         })
 
         it("Confirm deposit successful", async () => {
             const [owner] = await ethers.getSigners();
             let contractBalanceAfterDeposit = contractBalanceBeforeDeposit + amountToDeposit;
-            expect(await contract.totalStakedInPool()).to.equal(contractBalanceAfterDeposit);
+            expect(await stak.totalStakedInPool()).to.equal(contractBalanceAfterDeposit);
         })
 
         it("Check user's staked amount updated", async () => {
             const [owner] = await ethers.getSigners();
-            let userInfo = await contract.userInfo(owner.address);
+            let userInfo = await stak.userInfo(owner.address);
             let userStakedAmount = Math.trunc(ethers.utils.formatEther(`${userInfo.amountStaked}`))
             expect(userStakedAmount).to.equal(Math.trunc(ethers.utils.formatEther(String(amountToDeposit))))
         })
@@ -70,7 +70,7 @@ describe("Staking Contract", async () => {
     describe("issue the tokens and withdraw Tokens", async () => {
         before("Transfer MyToken to the staking contract", async () => {
             const [owner] = await ethers.getSigners();
-            await perkTokenContract.transfer(contract.address, ethers.utils.parseEther('1000000000000000000000000000'));
+            await perkTokenContract.transfer(stak.address, ethers.utils.parseEther('1000000000000000000000000000'));
             // wait for 30 sec
             function waitForTx(ms) {
                 return new Promise((res) => {
@@ -78,14 +78,14 @@ describe("Staking Contract", async () => {
                 });
             }
             await waitForTx(30000).then(async () => {
-                const tx = await contract.claimMyToken();
+                const tx = await stak.claimMyToken();
                 await tx.wait();
             })
         })
 
         it("withdraw the rewards within 1 month", async () => {
             const [owner] = await ethers.getSigners();
-            let userInfo = await contract.userInfo(owner.address);
+            let userInfo = await stak.userInfo(owner.address);
             let pendingReward = Number(((userInfo.amountStaked)*30)/(31536000)/1e4);
             expect(parseFloat(userInfo.bonusTokens)).to.be.above(pendingReward);
         })
@@ -94,7 +94,7 @@ describe("Staking Contract", async () => {
             const [owner] = await ethers.getSigners();
             let myTokenBalance = await myTokenContract.balanceOf(owner.address);
             let amountToWithdraw = ethers.utils.parseEther('90')
-            await contract.unstake(amountToWithdraw);
+            await stak.unstake(amountToWithdraw);
             let myTokenCurrentBalance = await myTokenContract.balanceOf(owner.address);
             let expectedStablecoinBalance = parseInt((myTokenBalance)) + parseInt(amountToWithdraw);
             // console.log({"before bal": myTokenBalance, "expected": expectedStablecoinBalance});
@@ -110,9 +110,9 @@ describe("Staking Contract", async () => {
             const [owner] = await ethers.getSigners();
 
             amountToDeposit = ethers.utils.parseEther('90');
-            await contract.depositInPool(amountToDeposit);
+            await stak.depositInPool(amountToDeposit);
 
-            await perkTokenContract.transfer(contract.address, ethers.utils.parseEther('100000000000000000000000'));
+            await perkTokenContract.transfer(stak.address, ethers.utils.parseEther('100000000000000000000000'));
             // wait for 90 sec
             function waitForTx(ms) {
                 return new Promise((res) => {
@@ -121,14 +121,14 @@ describe("Staking Contract", async () => {
             }
             await waitForTx(90000).then(async () => {
                 myTokenBalance = await myTokenContract.balanceOf(owner.address);
-                const tx = await contract.unstake(amountToDeposit);
+                const tx = await stak.unstake(amountToDeposit);
                 await tx.wait();
             })
         })
 
         it("Unstaking the tokens after an month", async () => {
             const [owner] = await ethers.getSigners();
-            let userInfo = await contract.userInfo(owner.address);
+            let userInfo = await stak.userInfo(owner.address);
             let pendingReward = Number(((userInfo.amountStaked)*(500)*90)/(31536000)/1e4);
             expect(parseFloat(userInfo.bonusTokens)).to.be.above(pendingReward);
         })
@@ -149,9 +149,9 @@ describe("Staking Contract", async () => {
             const [owner] = await ethers.getSigners();
 
             amountToDeposit = ethers.utils.parseEther('150');
-            await contract.depositInPool(amountToDeposit);
+            await stak.depositInPool(amountToDeposit);
 
-            await perkTokenContract.transfer(contract.address, ethers.utils.parseEther('1000000000000000000000000'));
+            await perkTokenContract.transfer(stak.address, ethers.utils.parseEther('1000000000000000000000000'));
             // wait
             function waitForTx(ms) {
                 return new Promise((res) => {
@@ -160,14 +160,14 @@ describe("Staking Contract", async () => {
             }
             await waitForTx(90000).then(async () => {
                 myTokenBalance = await myTokenContract.balanceOf(owner.address);
-                const tx = await contract.unstake(amountToDeposit);
+                const tx = await stak.unstake(amountToDeposit);
                 await tx.wait();
             })
         })
 
         it("Bonus perks for staking more than 100$", async () => {
             const [owner] = await ethers.getSigners();
-            let userInfo = await contract.userInfo(owner.address);
+            let userInfo = await stak.userInfo(owner.address);
             let pendingReward = Number(((userInfo.amountStaked)*(200)*(300)*90)/(31536000)/1e4);
             expect(parseFloat(userInfo.bonusTokens)).to.be.above(pendingReward);
         })
